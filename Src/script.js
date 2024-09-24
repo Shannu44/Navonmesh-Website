6ce2d635e18b72d7ad707960a48c0022326b8f44
Shannu44 <shanmukhakabbinagadde@gmail.com>
2024-09-24 18:33:27 +0530

    updated anim
    updated

---
+++ Src/script.js
@@ -0,0 +1,15 @@
+const hero = document.querySelector('.hero');
+const skyscraper = document.querySelector('.skyscraper');
+const skyscraper2 = document.querySelector('.skyscraper2');
+
+// Add event listener to the window scroll event
+window.addEventListener('scroll', () => {
+  // Get the current scroll position
+  const scrollPosition = window.scrollY;
+
+  hero.querySelector('h1').style.transform = `translateY(-${scrollPosition / 8}px)`;
+  
+  // Move the skyscrapers left and right
+  skyscraper.style.transform = `translate(${scrollPosition / 1}px, 0)`;
+  skyscraper2.style.transform = `translate(-${scrollPosition / 1}px, 0)`;
+});
