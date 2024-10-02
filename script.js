1132c6a30c6d53942b161efe2703e4d9f24f740f
Shannu44 <shanmukhakabbinagadde@gmail.com>
2024-10-02 17:02:04 +0530

    @5pm

--- script.js
+++ script.js
@@ -38,12 +38,3 @@ function hideSidebar(){
   const sidebar = document.querySelector('.sidebar');
   sidebar.style.display = 'none';
 }
-
-function showSidebar(){
-  const sidebar = document.querySelector('.sidebar')
-  sidebar.style.display = 'flex'
-}
-function hideSidebar(){
-  const sidebar = document.querySelector('.sidebar')
-  sidebar.style.display = 'none'
-}
