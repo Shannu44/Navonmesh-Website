8101165732afaa2b2866807d30d5e3122d79dd6f
Shannu44 <shanmukhakabbinagadde@gmail.com>
2024-10-10 10:46:44 +0530

    updated

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ window.addEventListener("load", function() {
   setTimeout(function() {
     loader.style.display = "none";       // Hide the loader
     window.scrollTo(0, 0);               // Scroll the page to the top after loading
-  }, 5000); // 5-second delay
+  }, 4000); // 5-second delay
 });
 
 
